8b86f955922557241e8871499a04f71b379fb99c
Seungwoo Lee <tmddn0325@gmail.com>
2023-09-01 17:06:34 +0900

    Added Enemy Random Script

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -4,19 +4,29 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public List<GameObject> enemies;
     public GameObject asteroid;
     public float time = 0;
-    public float maxTime = 3;
-    float asteroidGenSpeed;
+    public float spawnTime = 3;
+    float spawnSpeed;
     void Update()
     {
-        asteroidGenSpeed = Random.Range(1.0f, 5.0f);
-        time += Time.deltaTime * asteroidGenSpeed;
+        spawnSpeed = Random.Range(1.0f, 5.0f);
+        time += Time.deltaTime * spawnSpeed;
 
-        if(time > maxTime)
+        if(time > spawnTime)
         {
             time = 0;
-            Instantiate(asteroid, new Vector3(Random.Range(9.0f, 12.0f), Random.Range(-4.0f, 4.0f), 0), Quaternion.identity);
+            int check = Random.Range(0, 2);
+            if(check == 0)
+            {
+                Instantiate(asteroid, new Vector3(Random.Range(9.0f, 12.0f), Random.Range(-4.0f, 4.0f), 0), Quaternion.identity);
+            }
+            else
+            {
+                int type = Random.Range(0, 2);
+                Instantiate(enemies[type], new Vector3(Random.Range(9.0f, 12.0f), Random.Range(-4.0f, 4.0f), 0), Quaternion.identity);
+            }
         }
     }
 }
